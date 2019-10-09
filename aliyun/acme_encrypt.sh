aad0cd8dc8202be2bade4f74b6cc36bc0eaba2bc
hatlonely <hatlonely@gmail.com>
2019-10-10 01:16:46 +0800

    update

---
+++ aliyun/acme_encrypt.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+function encrypt() {
+    acme.sh --issue -d $1 --nginx
+    mkdir -p /etc/nginx/ssl/$1/
+    acme.sh --installcert -d $1 \
+        --key-file /etc/nginx/ssl/$1/$1.key \
+        --fullchain-file /etc/nginx/ssl/$1/fullchain.cer \
+        --reloadcmd  "service nginx force-reload"
+}
+
+encrypt blog.hatlonely.com
+encrypt jenkins.hatlonely.com
+encrypt gitlab.hatlonely.com
+encrypt grafana.hatlonely.com
+encrypt chronograf.hatlonely.com
+encrypt account.hatlonely.com
+encrypt api.account.hatlonely.com
+encrypt api.cloud.hatlonely.com
+encrypt api.ancient.hatlonely.com
+encrypt tool.hatlonely.com
+encrypt ancient.hatlonely.com
+encrypt game.hatlonely.com
